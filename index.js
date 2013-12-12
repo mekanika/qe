e0ed369b99ee658b74578fd1e29a60b7d07829c0
Clint Walker <clint@neonrelic.com>
2013-12-12 18:00:09 +0800

    Initial commit - 0.1.0

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/**
+ * NOT IMPLEMENTED - Qo - Query object validator
+ *
+ * @param {Qo} qo The query object to validate
+ *
+ * @returns {Error} Not implemented
+ */
+
+module.exports = function ( qo ) {
+  return new Error('Qo validation not implements');
+};
